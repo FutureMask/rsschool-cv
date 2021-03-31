ed58aa2a06f6c94341d4c0c4af3dffa5e6700504
FutureMask <56609336+FutureMask@users.noreply.github.com>
2021-04-01 00:48:15 +0300

    refactor: adjust formatting according to rule 2.1 "BEM" in html-and-css.md

--- clean-code-s1e1/app.js
+++ clean-code-s1e1/app.js
@@ -11,7 +11,7 @@
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var completedTasksHolder=document.getElementById("compl-tasks");//completed-tasks
 
 
 //New task list item
@@ -85,11 +85,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
-    //If class of the parent is .editmode
+    var containsClass=listItem.classList.contains("menu__item_editMode");
+    //If class of the parent is .menu__item_editMode
     if(containsClass){
 
-        //switch to .editmode
+        //switch to .menu__item_editMode
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +98,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    //toggle .menu__item_editMode on the parent.
+    listItem.classList.toggle("menu__item_editMode");
 };
 
 
